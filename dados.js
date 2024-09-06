494fed983d766b6cf801a38ce2e51ea059de0d4d
CirleiDias <cirlei.gestora@outlook.com>
2024-09-06 19:48:30 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados=[
+    {
+        titulo:"Brasil", 
+        descrição:"Historicamente, o Brasil é um dos países que mais sofre com queimadas, especialmente na Amazônia, Cerrado e Pantanal. Em 2024, o país registrou um aumento significativo no número de focos de incêndio, principalmente nas regiões Centro-Oeste e Norte.",
+        link:"https://pt.wikipedia.org/wiki/Queimada#:~:text=Ao%20contr%C3%A1rio%20do%20que%20preconizam%20os%20estudiosos%20e,cultivo%20monocultor%20etc.%2C%20que%20dizimaram%20a%20flora%20nativa",
+        tags:[
+            "pais:brasil",
+            "região:amazônia",
+            "região:cerrado",
+            "região:pantanal",
+            "problema:queimadas",
+            "causa:desmatamento",
+            "dado:histórico"
+        ] 
+    },
+    {
+        titulo:"Rangink", 
+        descrição:"Situação Atual Dados: Apenas Satélite de Referência - AQUA Tarde ",
+        link:"https://terrabrasilis.dpi.inpe.br/queimadas/situacao-atual/situacao_atual/",
+        tags: [
+            "ferramenta:satélite",
+            "satélite:aqua",
+            "monitoramento:queimadas",
+            "dado:real-time",
+            "fonte:inpe"
+        ]
+    },
+    {   
+        titulo:"Focos Ativos", 
+        descrição:"Monitoramento dos Focos Ativos por Países",
+        link:"https://terrabrasilis.dpi.inpe.br/queimadas/situacao-atual/estatisticas/estatisticas_paises/",
+         tags:[
+            "dado:estatística",
+            "tema:queimadas",
+            "escopo:global",
+            "fonte:inpe"
+        ]
+    }, 
+]; 
